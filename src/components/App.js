ee0f196c6c61ad86694f603202dbc657e01ea8a7
Elan Zinkow <ezinkow@gmail.com>
2021-02-06 13:20:29 -0600

    display roster on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import '../App.css';
 import Welcome from './Welcome'
+import {playerData} from '../data/playerdata';
+import PlayerList from './PlayerList'
+
+// console.log("players", playerData)
 
 function App() {
   return (
     <div className="container">
       <Welcome />
+      <PlayerList players={playerData}/>
     </div>
   );
 }
