82c5ad3e55b64582bf96f75747e48a2d25273d20
Elan Zinkow <ezinkow@gmail.com>
2021-02-06 14:06:12 -0600

    adding time related greeting

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,16 @@
 function Welcome() {
+    const currentHour = new Date().getHours()
+    const message = currentHour < 5 ? "why are you still awake dumbfuck. go to sleep"
+        : currentHour < 12 ? "good morning"
+        : currentHour < 18 ? "good afternoon"
+        : "good night"
+
     return (
-    <h2>Welcome baseball fans!</h2>
-        )
+        <>
+            <h4>{message} bitches</h4>
+            <h2>Welcome baseball fans!</h2>
+        </>
+    )
 }
 
 export default Welcome
